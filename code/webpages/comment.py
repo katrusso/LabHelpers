dc6d410402b9dbe5ed95dc2074a6b1b65f105451
trillisk <njsstamm@gmail.com>
2014-10-26 13:08:09 -0400

    Comment page created guestbook style
    This will needed to be changed for the switching between comments pages.
    Don't let users create their own, some list or drop down

--- code/webpages/comment.py
+++ code/webpages/comment.py
@@ -6,6 +6,83 @@ from google.appengine.ext import ndb
 
 import webapp2
 
+MAIN_PAGE_FOOTER_TEMPLATE = """\
+    <form action="/sign?%s" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Leave Comment"></div>
+    </form>
+    <hr>
+    <form>Comments page:
+      <input value="%s" name="Comments Pages">
+      <input type="submit" value="switch">
+    </form>
+    <a href="%s">%s</a>
+  </body>
+</html>
+"""
+
+MAXCOMMENT = 50
+DEFAULT_GUESTBOOK_NAME = 'General Comments'
+
+# We set a parent key on the 'Greetings' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+    return ndb.Key('Guestbook', guestbook_name)
+
+class Greeting(ndb.Model):
+    """Models an individual Guestbook entry."""
+    author = ndb.UserProperty()
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
 class CommentPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.write("Leave Comments Here")
+  def get(self):
+        self.response.write('<html><body>')
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(MAXCOMMENT)
+
+        for greeting in greetings:
+            if greeting.author:
+                self.response.write(
+                        '<b>%s</b> wrote:' % greeting.author.nickname())
+            else:
+                self.response.write('An anonymous person wrote:')
+            self.response.write('<blockquote>%s</blockquote>' %
+                                cgi.escape(greeting.content))
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        # Write the submission form and the footer of the page
+        sign_query_params = urllib.urlencode({'guestbook_name': guestbook_name})
+        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE %
+                            (sign_query_params, cgi.escape(guestbook_name),
+                             url, url_linktext))
+        
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        guestbook_name = self.request.get('guestbook_name',DEFAULT_GUESTBOOK_NAME)
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
+
+
+        
