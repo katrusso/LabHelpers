c857fa76dd51efac08a1141b6364cec10a0bff7a
Gerrett Diamond <diamog@rpi.edu>
2014-10-26 07:25:50 -0400

    built framework for inheritance

--- code/webpages/labpages.py
+++ code/webpages/labpages.py
@@ -9,27 +9,22 @@ import webapp2
 import questions
 from html import *
 
-class StaticLabPage(webapp2.RequestHandler):
-    #Gets the id of the static lab from the url
+class LabPage(webapp2.RequestHandler):
+    #Gets the id of the lab from the url
     def getLabID(self):
         my_url = self.request.uri
         lab_id = my_url[len(my_url)-3:len(my_url)-1]
         return int(lab_id)
 
-    #function that is run at beginning of webpage
     def get(self):
         self.num=0
         #starts off the webpage with a form
         self.response.write(OPEN_HTML.substitute(head=""))
         self.response.write(FORM_HTML.substitute(action="",method="post"))
-
-        #gets the questions for the specific lab ID
-        lab_id = self.getLabID()
-        questions_query = questions.Question.query(
-            ancestor=questions.lab_key(lab_id)).order(questions.Question.number)
-        question_list = questions_query.fetch()
+        self.gatherQuestions()
+        
         #run through each question creating a multiple choice question for it
-        for question in question_list:
+        for question in self.question_list:
             self.num=self.num+1
             self.response.write(str(self.num)+". ")
             self.response.write(question.question)
@@ -49,16 +44,13 @@ class StaticLabPage(webapp2.RequestHandler):
         #end of page
         self.response.write("</form>")
         self.response.write(CLOSE_HTML)
-
+    
     #After submission page
     def post(self):
         #query for the number of questions
-        questions_query = questions.Question.query(
-            ancestor=questions.lab_key(self.getLabID())).order(questions.Question.number)
-        question_list = questions_query.fetch()
-
+        self.gatherQuestions()
         #checks if each answer is correct or wrong
-        for i in range(len(question_list)):
+        for i in range(len(self.question_list)):
             if self.request.get("q"+str(i+1))=="correct":
                 self.response.write("correct")
             else:
@@ -66,7 +58,15 @@ class StaticLabPage(webapp2.RequestHandler):
             self.response.write("<br>")
 
 
-class DynamicLabPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('dynamic meow<br>')
-        self.response.write(self.request.uri)
+
+class StaticLabPage(LabPage):
+    def gatherQuestions(self):
+        #gets the questions for the specific lab ID
+        lab_id = self.getLabID()
+        questions_query = questions.Question.query(
+            ancestor=questions.lab_key(lab_id)).order(questions.Question.number)
+        self.question_list = questions_query.fetch()
+
+class DynamicLabPage(LabPage):
+    def gatherQuestions(self):
+        self.question_list=[]
