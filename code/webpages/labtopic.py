9bf699314d05b3ea7d32ecbc6df6f0dc839f2f72
katrusso <russok2@rpi.edu>
2014-11-19 00:47:36 -0500

    finished commenting/verifying coding standards

--- code/webpages/labtopic.py
+++ code/webpages/labtopic.py
@@ -13,10 +13,23 @@ MAX_CHARACTERS = 80
 
 class LabTopic(webapp2.RequestHandler):
     '''
-    This class 
+    As seen in labpages.py, after the user submits their completed lab a small dashboard
+    of their grades per section appears at the top of the corrected lab. 
+    
+    Within the dashboard is a tabular list of topics; they are clickable links.
+    Below them the user's grade for questions associated with that topic.
+    
+    The purpose of this class is to open the "coach data" webpage in the same tab when a 
+    user clicks on a topic-link in the grade-dashboard of their corrected lab. 
+    
+    The coach data contains the topic name, relevant formulas, an explanation of the 
+    topic's fundamental concepts, and an example. 
+    
+    The intention here being to help the student recognize the source of their errors 
+    without requiring additional resources. 
     '''
     
-    def get(self):
+    def get(self):                                                                  #FETCH COACH DATA ASSOCIATED WITH TOPIC LINK
         my_url = self.request.uri
         ind = my_url.rfind('/')
         topic_name = my_url[ind+1:len(my_url)]
@@ -27,17 +40,16 @@ class LabTopic(webapp2.RequestHandler):
             return;
         coach_object = coach_query.fetch()[0]
         
-        self.response.write(OPEN_HTML.substitute(head='''<link type="
-        text/css" rel="stylesheet" href="/stylesheets/labtopic.css" 
-            />'''))
+        self.response.write(OPEN_HTML.substitute(head='''<link 
+        rel="stylesheet" href="/stylesheets/labtopic.css"/>'''))                    #STYLESHEET - MAIN
         
-        write_css_html(self,"<b>"+topic_name+"</b>")    #inserts page heading (image + labhelpers)
+        write_css_html(self,"<b>"+topic_name+"</b>")                                #INSERTS PAGE HEADING FROM HTML CONSTANTS FILE (ICON + "LAB HELPERS")
         
         
-        self.response.write(CSS_CLASS_HTML.substitute(id="coach-body"))
-        self.response.write(CSS_CLASS_HTML.substitute(id="coach-section"))
+        self.response.write(CSS_CLASS_HTML.substitute(id="coach-body"))             #STYLESHEET - OPEN TAG :: COACH-BODY
+        self.response.write(CSS_CLASS_HTML.substitute(id="coach-section"))          #STYLESHEET - OPEN TAG :: COACH-SECTION
 
-        self.response.write("<br><br>Relevent Equations:<br>")
+        self.response.write("<br><br>Relevant Equations:<br>")
         for i in coach_object.equations:
             self.response.write(i)
             self.response.write("<br>")
@@ -47,8 +59,8 @@ class LabTopic(webapp2.RequestHandler):
                                                      text="Return to Lab"))
         self.response.write(CLOSE_HTML)
         
-        self.response.write(CLOSE_CSS_HTML)#coach-section
-        self.response.write(CLOSE_CSS_HTML)#coach-body
+        self.response.write(CLOSE_CSS_HTML)                                         #STYLESHEET - CLOSING TAG :: COACH-SECTION
+        self.response.write(CLOSE_CSS_HTML)                                         #STYLESHEET - CLOSING TAG :: COACH-BODY
 
         
     def __print__(self,string):
