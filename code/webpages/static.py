5709cfbdde84684c32a99673c38641f6ae167ce9
katrusso <russok2@rpi.edu>
2014-11-20 12:37:48 -0500

    updated comments on static.py #20306739664924

--- code/webpages/static.py
+++ code/webpages/static.py
@@ -1,45 +1,52 @@
 import os
 import urllib
 import cgi
-from google.appengine.api import users
-from google.appengine.ext import ndb
+from google.appengine.api import users                          
+from google.appengine.ext import ndb                                                #ENABLES INTERACTION WITH DATASTORE
 
-import webapp2
+import webapp2                                                                      #FRAMEWORK
 
-import lab
+import lab                                                                          #RELEVANT CLASSES
 import questions
 import userclass
 import labpages
-from html_constants import *
+from html_constants import *                                                        #REDUCES CODE REDUNDANCIES
 
-#Implements the gatherQuestions function to select questions based on 
-#lab id
+
 class StaticLabPage(labpages.LabPage):
-    #Gets the id of the lab from the url
-    def __get_labID__(self):
+    '''
+    THIS CLASS IMPLEMENTS THE GATHER-QUESTIONS FUNCTION TO SELECT LAB QUESTIONS FROM THE 
+    DATASTORE BASED ON THE LAB ID (RETRIEVED FROM THE LAB URL USER CLICKS THROUGH ON 
+    MAINPAGE)
+    '''
+    def __get_labID__(self):                                                        #GETS THE ID OF THE LAB FROM THE URL
         my_url = self.request.uri
         ind = my_url[0:len(my_url)-1].rfind('/')
         lab_id = my_url[ind+1:(len(my_url)-1)]
         return int(lab_id)
-    def __get_lab_name__(self):
+        
+    def __get_lab_name__(self):                                                     #GETS LAB NAME
         lab_query = lab.Lab.query(ancestor=lab.lab_key(1)).order(lab.Lab.id)
         lab_list = lab_query.fetch()
         for i in lab_list:
             if i.id==self.__get_labID__():
                 lab_object=i
         return lab_object.name
-    def __write_header__(self):
+        
+    def __write_header__(self):                                                     #POPULATES PAGE HEADER
         lab_name = self.__get_lab_name__()
         write_css_html(self,"Lab "+str(self.__get_labID__())+
             ": "+lab_name) 
-    def __get_responses__(self,user_object):
+            
+    def __get_responses__(self,user_object):                                        #GETS USER RESPONSES
         username = users.get_current_user()
         return user_object[0].__query_responses__(self.__get_labID__(),username.nickname())
-    def __add_responses__(self,user_object,lab_id,responses,correct):
+        
+    def __add_responses__(self,user_object,lab_id,responses,correct):               #SAVES USER RESPONSES
         username = users.get_current_user()
         user_object[0].__add_responses__(lab_id,username.nickname(),responses,correct)
-    def __gather_questions__(self,sort_by_topic):
-        #gets the questions for the specific lab ID
+        
+    def __gather_questions__(self,sort_by_topic):                                   #GETS THE QUESTIONS ASSOCIATED WITH THE SPECIFIC LAB ID
         lab_id = self.__get_labID__()
         questions_query = questions.Question.query(
             ancestor=questions.lab_key(lab_id))
